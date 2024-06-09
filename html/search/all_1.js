5ea209be36cf9b7d3470bbe7a69070e3c931fbc9
Yu Heng <ngyuheng08@gmail.com>
2024-06-09 23:55:23 +0800

    🔥 clean up

--- html/search/all_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['cats_3a_20particle_20cluster_20approximation_20tree_20simulation_0',['P-CATS: Particle-Cluster Approximation Tree Simulation',['../md__r_e_a_d_m_e.html',1,'']]],
-  ['cluster_20approximation_20tree_20simulation_1',['P-CATS: Particle-Cluster Approximation Tree Simulation',['../md__r_e_a_d_m_e.html',1,'']]]
-];
--- html/search/all_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['tree_20simulation_0',['P-CATS: Particle-Cluster Approximation Tree Simulation',['../md__r_e_a_d_m_e.html',1,'']]]
-];
