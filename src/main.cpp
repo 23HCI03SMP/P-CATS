85853785badcb180bf417168de1fdfbb03b2064a
UnidentifiedX <80146445+UnidentifiedX@users.noreply.github.com>
2024-06-09 17:48:42 +0000

    Deploying to gh-pages from @ 23HCI03SMP/P-CATS@24107b5dcb5493c2583dfcb6ec4efa2b396f183a 🚀

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/// A structure to store force components (in Newtons).
+///
+/// This structure is used to store the components of a force in Newtons.
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
