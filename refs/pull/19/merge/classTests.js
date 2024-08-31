e053aeedf587d12c1091ac436e8cab6b6b7a6867
ngyuheng <ngyuheng@users.noreply.github.com>
2024-08-31 03:08:52 +0000

    Deploying to gh-pages from @ 23HCI03SMP/P-CATS@2ecfec3ab4c9624dd780346a1510d0a63042cb97 🚀

---
+++ refs/pull/19/merge/classTests.js
@@ -0,0 +1,5 @@
+var classTests =
+[
+    [ "Tests", "classTests.html#a59bb27f94deafdca97cec0585136a713", null ],
+    [ "run_all_tests", "classTests.html#a32b139c9d58430630bf3a63ca2f94089", null ]
+];
---
+++ refs/pull/19/merge/structPoint.js
@@ -0,0 +1,12 @@
+var structPoint =
+[
+    [ "Point", "structPoint.html#ad92f2337b839a94ce97dcdb439b4325a", null ],
+    [ "Point", "structPoint.html#a4d43f5247afe8c85c6da1aa39dbcc738", null ],
+    [ "magnitude", "structPoint.html#ae061fb2b6e0125058250aa2f80d1136f", null ],
+    [ "operator+", "structPoint.html#a4218ebbe1cc5d02a823c49e3fc9c0703", null ],
+    [ "operator-", "structPoint.html#ae4ec92b14e766b962f09000aff318f48", null ],
+    [ "operator==", "structPoint.html#a3618cc973762afde2e8df59bff4a86ca", null ],
+    [ "x", "structPoint.html#ab99c56589bc8ad5fa5071387110a5bc7", null ],
+    [ "y", "structPoint.html#afa38be143ae800e6ad69ce8ed4df62d8", null ],
+    [ "z", "structPoint.html#a05ba3b1dfcb19430582ae953cbbfbded", null ]
+];
