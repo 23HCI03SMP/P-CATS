43862b1bd41c4720f091679b4b48bb4e7c9fa9f9
Yu Heng <ngyuheng08@gmail.com>
2024-08-31 11:06:22 +0800

    ✨ created a timer

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,46 @@
 #include "include/node.h"
 #include "include/interactions.h"
 #include "include/tests.h"
+#include <chrono>
 
-/// A structure to store force components (in Newtons).
-///
-/// This structure is used to store the components of a force in Newtons.
+
+class Timer
+{
+public:
+    Timer()
+    {
+        start = std::chrono::high_resolution_clock::now();
+    }
+
+    void end()
+    {
+        end_time = std::chrono::high_resolution_clock::now();
+        duration = end_time - start;
+        float ms = duration.count();
+        std::cout << "Elapsed Time: " << ms << "ms" << std::endl << std::endl;
+    }
+
+    ~Timer()
+    {
+        end();
+    }
+
+private:
+    std::chrono::time_point<std::chrono::system_clock> start;
+    std::chrono::time_point<std::chrono::system_clock> end_time;
+    std::chrono::duration<float, std::milli> duration = end_time - start;
+};
+
+
 int main()
 {
+    Timer overallTimer;
+
+    // Remove old positions file
+    remove("./viewer/positions.csv");
+
+    // Create space
+    Timer generationTimer;
     Space *space = new Space(Point(0, 0, 0), Point(10, 10, 10));
     std::tuple<Particle, double> p1 = std::make_tuple(Particle("Proton", 1.67e-27, Charge(1.602e-19, 0)), 0.1);
     std::tuple<Particle, double> p2 = std::make_tuple(Particle("Electron", 9.11e-31, Charge(0, -1.602e-19)), 0.1);
@@ -18,74 +52,35 @@ int main()
     std::vector<std::tuple<Particle, double>> particles = {p1, p2};
     space->generateParticles(4, 300, particles, HotspotShape::SPHERE, {4});
 
-    // Particle *p1 = new Particle("a", 1, Charge(), Point(0, 0, 0)); // bottom left front
-    // Particle *p2 = new Particle("b", 1, Charge(), Point(1, 1, 1)); // top right back
-    // Particle *p3 = new Particle("c", 1, Charge(), Point(0.2, 0.2, 0.2)); // centre
-    // Particle *p4 = new Particle("d", 1, Charge(), Point(0.8, 0.8, 0.8)); // centre
-    // Particle *p5 = new Particle("e", 1, Charge(), Point(0.25, 0.25, 0.25)); // centre
-    // Particle *p6 = new Particle("f", 1, Charge(), Point(0.75, 0.75, 0.75)); // centre
-    // Particle *p7 = new Particle("g", 1, Charge(), Point(0.5, 0.5, 0.5)); // centre
-    // Particle *p8 = new Particle("h", 1, Charge(), Point(0.1, 0.2, 0.8)); // centre
-    // Particle *p9 = new Particle("i", 1, Charge(), Point(0.3, 0.3, 0.3)); // centre
-    // Particle *p10 = new Particle("j", 1, Charge(), Point(0.9, 0.9, 0.9)); // centre
-    // Particle *p11 = new Particle("k", 1, Charge(), Point(0.4, 0.4, 0.4)); // centre
-    // Particle *p12 = new Particle("l", 1, Charge(), Point(0.45, 0.45, 0.45)); // centre
-    // Particle *p13 = new Particle("m", 1, Charge(), Point(0.42, 0.42, 0.42)); // centre
-
-    // Space *baseSpace = new Space(Point(0, 0, 0), Point(1, 1, 1));
-
-    // baseSpace->insert(p1);
-    // baseSpace->insert(p2);
-    // baseSpace->insert(p3);
-    // baseSpace->insert(p4);
-    // baseSpace->insert(p5);
-    // baseSpace->insert(p6);
-    // baseSpace->insert(p7);
-    // baseSpace->insert(p8);
-    // baseSpace->insert(p9);
-    // baseSpace->insert(p10);
-    // baseSpace->insert(p11);
-    // baseSpace->insert(p12);
-    // baseSpace->insert(p13);
-
     // Create main loop
+    double dt = 1e-5;
+    int timeSteps = 10;
+    double theta = 0.5;
 
-    Force force = Force(0, 0, 0);
+    std::cout << "Generating Particles..." << std::endl;
+    space->toFile(0, "./viewer/positions.csv");
+    generationTimer.end();
 
     Interactions interactions;
-
-    int timeSteps = 5;
-
-
     for (int i = 0; i < timeSteps; i++) {
-        space->toFile(i, "./viewer/positions.csv");
-        std::cout << space->getAllParticles().size() << std::endl;
+        Timer timer;
+        std::cout << "Time step: " << i + 1 << std::endl;
         Space *newSpace = new Space(Point(0, 0, 0), Point(10, 10, 10));
         for (auto part : space->getAllParticles())
         {
-            newSpace->insert(interactions.Interact(*part, space, 0.5, 100));
+            newSpace->insert(interactions.Interact(*part, space, theta, dt));
         }
         newSpace->recalculateCentreOfCharge();
-        std::cout << newSpace->getAllParticles().size() << std::endl;
-        std::cout << "Time step: " << i << std::endl;
-        // std::cout << "======================================================================================================" << std::endl;
-        // std::cout << newSpace->toString() << std::endl;
         delete space;
         space = newSpace;
+        space->toFile(i, "./viewer/positions.csv");
     }
-    space->toFile(timeSteps, "./viewer/positions.csv");
-    // delete baseSpace;
-    // baseSpace = newTree;
-    // delete newTree;
 
-    // delete baseSpace;
+    std::cout << "Simulation complete." << std::endl;
+    overallTimer.end();
+
+    // Run viewer
+    system("cd viewer && python viewer.py runserver -d");
     
-
-    Space *newTree = new Space(Point(0, 0, 0), Point(10, 10, 10));
-    newTree->insert(new Particle("Proton", 1.67e-27, Charge(1.602e-19, 0), Point(0, 0, 0)));
-
-    // std::cout << baseSpace->toString() << std::endl;
-    // baseSpace->toFile(0, "./viewer/positions.csv");
-    system("cd viewer && python viewer.py");
     return 0;
 }
